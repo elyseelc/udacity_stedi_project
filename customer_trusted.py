38af1cf2cd2791ca3a367c2c73ce66509789f162
elyseelc <128007448+elyseelc@users.noreply.github.com>
2023-06-23 13:59:42 +1000

    Issue with customer_trusted.py

--- customer_trusted.py
+++ customer_trusted.py
@@ -1,1 +1,3 @@
 #Python script that sanitises customer data, creating customer_trusted
+
+# ISSUE: The scrpit has not filtered out the untrusted customers for some reason (confirmed with Athena query)
