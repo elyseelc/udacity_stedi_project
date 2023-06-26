b9fab5dafdcac979d833f1bc1b46a8f7fa7f14ea
elyseelc <128007448+elyseelc@users.noreply.github.com>
2023-06-26 14:51:08 +1000

    Update customer_trusted.py

--- customer_trusted.py
+++ customer_trusted.py
@@ -1,12 +1,9 @@
-#Python script that sanitises customer data, creating customer_trusted
 import sys
 from awsglue.transforms import *
 from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
-import logging
-from awsglue.utils import getResolvedOptions
 
 ## @params: [JOB_NAME]
 args = getResolvedOptions(sys.argv, ['JOB_NAME'])
@@ -15,26 +12,15 @@ glueContext = GlueContext(sc)
 spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
-logger = logging.getLogger(args['JOB_NAME'])
-logger.setLevel(logging.INFO)
 
-datasource0 = glueContext.create_dynamic_frame.from_catalog(database="stediproject_database2", table_name="stediprojectbucket_customerlanding1")
-
-# Print sample data from the shareWithResearchAsOfDate column
-datasource0.toDF().select("shareWithResearchAsOfDate").show(10)
-
-# Print total rows before filter
-logger.info("This is a print statement Total rows before filter: {}".format(datasource0.count()))
+datasource0 = glueContext.create_dynamic_frame.from_catalog(database = "stediproject_database2", table_name = "customer_landing")
 
 PrivacyFilter_node2 = Filter.apply(
-    frame=datasource0,
-    f=lambda row: (row["shareWithResearchAsOfDate"] != ""),
-    transformation_ctx="PrivacyFilter_node2"
+frame=datasource0,
+f=lambda row: (not (row["shareWithResearchAsOfDate"] == 0)),
+transformation_ctx="PrivacyFilter_node2",
 )
 
-# Print total rows after filter
-logger.info("This is a print statement Total rows after filter: {}".format(PrivacyFilter_node2.count()))
-
-glueContext.write_dynamic_frame.from_options(PrivacyFilter_node2, connection_type="s3", connection_options={"path": "s3://stediprojectbucket-customerlanding1/customer_trusted"}, format="parquet", transformation_ctx="datasink2")
+glueContext.write_dynamic_frame.from_options(PrivacyFilter_node2, connection_type = "s3", connection_options = {"path": "s3://stediprojectbucket-customerlanding1/customer_trusted"}, format = "parquet", transformation_ctx = "datasink2")
 
 job.commit()
